46ae2436774668810646bf62e6afbec75bbeaa6a
Ronit Rameja <ronitrameja28@gmail.com>
2025-08-04 11:31:26 +0530

    Update LazyTrack formula to v1.0.0 with correct checksum

--- Formula/lazytrack.rb
+++ Formula/lazytrack.rb
@@ -1,11 +1,27 @@
 class Lazytrack < Formula
   desc "A fun CLI-based time/habit tracker"
   homepage "https://github.com/master-wayne7/lazytrack"
-  version "0.0.0"
+  version "1.0.0"
 
-  # This will be updated by GoReleaser
-  url "https://github.com/master-wayne7/lazytrack/releases/download/v0.0.0/lazytrack_Darwin_x86_64.tar.gz"
-  sha256 "placeholder"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/master-wayne7/lazytrack/releases/download/v1.0.0/lazytrack_Darwin_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/master-wayne7/lazytrack/releases/download/v1.0.0/lazytrack_Darwin_x86_64.tar.gz"
+      sha256 "E0EECC7010E5406AE40E3DF3E9D09E05627FC8FEE288505311C20289EAFF2D06"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/master-wayne7/lazytrack/releases/download/v1.0.0/lazytrack_Linux_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/master-wayne7/lazytrack/releases/download/v1.0.0/lazytrack_Linux_x86_64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
 
   def install
     bin.install "lazytrack"
